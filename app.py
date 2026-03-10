ce4640016852cf7f764b02a21a9f9206f2c49eeb
Dustin <dschones@gmail.com>
2026-03-09 22:12:39 -0700

    really hopefully the last - just envs

--- app.py
+++ app.py
@@ -239,7 +239,7 @@ def status():
 # ---------------------------------------------------------------------------
 if __name__ == '__main__':
     import os
-    debug = os.environ.get('FLASK_DEBUG', 'true').lower() == 'true'
-    host = os.environ.get('HOST', '127.0.0.1')
+    host = os.environ.get('HOST', '0.0.0.0')
     port = int(os.environ.get('PORT', 5000))
+    debug = os.environ.get('FLASK_DEBUG', 'false').lower() == 'true'
     app.run(host=host, port=port, debug=debug)
